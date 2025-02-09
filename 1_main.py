93316c69453ad7e808b635d073a90cf7ad79a357
MANIMSV <manikandan.2002121@srec.ac.in>
2025-02-09 19:28:03 +0530

    set window title and add a button to the main application

--- 1_main.py
+++ 1_main.py
@@ -2,6 +2,19 @@ import sys
 from PySide6.QtWidgets import *
 
 class button_holder(QMainWindow):
-    def __init__(slef):
+    def __init__(self):
         super().__init__()
+        self.setWindowTitle("First App! - Button Holder")
+        button = QPushButton("Click me!")
+
+        self.setCentralWidget(button)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = button_holder()
+    window.show()
+    app.exec()
+
+
+
         
