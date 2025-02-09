b6a7420ce97f99f3346e9532adf16512ddcf41c8
MANIMSV <manikandan.2002121@srec.ac.in>
2025-02-09 19:18:07 +0530

    update main application to use QMainWindow and add a button

--- 0_main.py
+++ 0_main.py
@@ -1,8 +1,12 @@
-from PySide6.QtWidgets import *     # Import the PySide6 module
-import sys                        # Import the sys module to get the command line arguments
+from PySide6.QtWidgets import *         # Import the PySide6 module
+import sys                              # Import the sys module to get the command line arguments
 
-app = QApplication(sys.argv)    # Create a new instance of QApplication
-window = QWidget()           # Create a new instance of a window
-window.show()             # Make the window visible
+app = QApplication(sys.argv)            # Create a new instance of QApplication
+window = QMainWindow()                  # Create a new instance of a window
+window.setWindowTitle("First App!")     # Set the window title
 
-app.exec()              # Start the event loop
+button = QPushButton("Click me!")       # Create a button with the text "Click me!"
+window.setCentralWidget(button)         # Set the central widget of the window to the button
+window.show()                           # Make the window visible
+
+app.exec()                              # Start the event loop
